06776b71d22e7f754ec93676031a1341fafe5b8c
Michael Carroll <carrollm2@gmail.com>
2017-11-14 22:43:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name){
   var kittens = ["Milo", "Otis", "Garfield"]
   kittens.push(name)
+
+  console.log("Hello", kittens)
   return kittens
 }
 
