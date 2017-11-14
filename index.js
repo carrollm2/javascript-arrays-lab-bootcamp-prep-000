b6906fdfa056d221e43b0983a2ae993d07f7de5c
Michael Carroll <carrollm2@gmail.com>
2017-11-14 22:27:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 const app = "I don't do much."
+
+const var kittens = ["Milo","Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  var kittens = ["Milo", "Otis", "Garfield"]
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  var kittens = ["Milo", "Otis", "Garfied"]
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  var kittens = ["Milo", "Otis", "Garfield"]
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  var kittens = ["Milo", "Otis", "Garfield"]
+  return kittens.shift()
+}
+
+function appendKitten(name){
+
+  var newArray = new Array();
+  newArray = [name, ...kittens]
+
+  return newArray
+}
+
+function prependKitten(name){
+
+  var newArray = new Array();
+
+  newArray = [...kittens, name]
+
+  return newArray
+}
+
+
+function removeLastKitten(){
+  var newArray = new Array();
+  newArray = kittens.slice(0, kittens.length - 1)
+  
+  return newArray
+}
+
+function removeFirstKitten(){
+  var newArray = new Array();
+  newArray = kittens.slice(1, kittens.length)
+
+  return newArray
+}
